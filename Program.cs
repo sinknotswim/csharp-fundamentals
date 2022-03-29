d7de550701752f33fa05de2f03ab67b81840fda6
sinknotswim <83678633+sinknotswim@users.noreply.github.com>
2022-03-29 14:39:27 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+This local function is for my first exercise lab.
+*/
+
+void MyLocalFunction()
+{
+    Console.WriteLine("This is my first sentence");
+Console.WriteLine("I am new to CSharp");
+Console.WriteLine("This CSharp course is cool");
+Console.WriteLine("I am learning stuff every day.");
+}
+
+MyLocalFunction();
