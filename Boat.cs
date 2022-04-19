a4a2ecd874851784d4ed100b405469d58da7154c
sinknotswim <83678633+sinknotswim@users.noreply.github.com>
2022-04-19 16:57:51 -0500

    Add files via upload

---
+++ Boat.cs
@@ -0,0 +1,36 @@
+﻿namespace Boat;
+class Boat
+{
+    private int _seats; //int declared
+    private int _speed;
+    private string _color;
+    public int Seats //int used
+    {
+        get => _seats;
+        set => _seats = value;
+    }
+    public int BoatSpeed
+    {
+        get => _speed;
+        set => _speed = value;
+    }
+    public string Color
+    {
+        get => _color;
+        set => _color = value;
+    }
+
+    public Boat(int Seats, int BoatSpeed, string Color)
+    {
+        Console.WriteLine(Seats);
+        Console.WriteLine(BoatSpeed);
+        Console.WriteLine(Color);
+    }
+    public Boat(int Seats, int BoatSpeed)
+    {
+        this.Seats = Seats;
+        this.BoatSpeed = BoatSpeed;
+    }
+    public Boat()
+        : this(4, 100, "Red") { }
+}
---
+++ Horse.cs
@@ -0,0 +1,38 @@
+﻿namespace MyFirstApplication;
+
+class Horse
+{
+    public string Breed { get; set; }
+    public int Speed { get; set; }
+    public string Coat { get; set; }
+    //breed, running speed, coat type, all 
+
+    public Horse(string Breed, int Speed, string Coat) //const, has same name as class
+    {
+        Console.WriteLine(Breed);
+        Console.WriteLine(Speed);
+        Console.WriteLine(Coat);
+    }
+
+    public Horse(int Speed, string Coat) //const 2 parameters, this keyword
+    {
+        this.Speed = Speed;
+        this.Coat = Coat;
+    }
+
+    public Horse()
+        : this("Shire", 100, "Shorthair") { }
+
+}
+class Program
+{
+    static void Main()
+    {
+        Horse one = new Horse("Arabian", 100, "Shorthair");
+        Horse two = new Horse(100, "Longhair");
+        Horse three = new Horse();
+        Boat first = new Boat(2, 50, "Blue");
+        Boat second = new Boat(4, 50);
+        Boat third = new Boat();
+    }
+}
