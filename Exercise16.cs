b20612b4e845a31df0dc18a8b0a97fba5b7105dc
sinknotswim <83678633+sinknotswim@users.noreply.github.com>
2022-04-22 02:05:17 -0500

    Add files via upload

---
+++ Exercise16.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise16;
+
+public class Exercise16
+{
+    public static void HorseList()
+    {
+        List<string> myHorseList = new List<string>();
+        myHorseList.Add("One Horse");
+        myHorseList.Add("Two Horse");
+        myHorseList.Add("Three Horse");
+        myHorseList.Add("Four Horse");
+
+        foreach (string horse in myHorseList)
+        {
+            Console.WriteLine(horse);
+        }
+    }
+    public static void MovieDictionary()
+    {
+        Dictionary<int, string> openWith = new Dictionary<int, string>();
+        openWith.Add(1, "The Thing");
+        openWith.Add(2, "Scream");
+        openWith.Add(3, "Ringu");
+        openWith.Add(4, "Conjuring");
+
+        foreach (KeyValuePair<int, string> pair in openWith)
+        {
+            Console.WriteLine($"For Key: {pair.Key} we have this Movie: {pair.Value}");
+        }
+    }
+    static void Main()
+    {
+        HorseList();
+        MovieDictionary();
+    }
+
+}
