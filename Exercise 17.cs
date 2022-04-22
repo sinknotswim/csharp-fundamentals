c0475560503f4658f47358ce4be48d1311071bc8
sinknotswim <83678633+sinknotswim@users.noreply.github.com>
2022-04-22 03:11:45 -0500

    Add files via upload

---
+++ Exercise 17.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercise17;
+
+public class Exercise17
+{
+
+    public static void Question1(int one, int two)
+    {
+        try
+        {
+            int three = one / two;
+            Console.WriteLine($"The first int is {one}, the second is {two}. This is the result of one/two: {three}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine($"Null reference: {ex.Message}");
+        }
+    }
+
+    public static void Question2(string Horse)
+    {
+        if(Horse == null)
+        {
+            throw new NullReferenceException("Horse is null.");
+        }
+        else
+        {
+            Console.Write(Horse);
+        }
+    }
+
+}
